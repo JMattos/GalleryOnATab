5f713d9bbcb548b9ff679a9374da775e073ad3a8
John Mattos <jmattos@Johns-MacBook-Pro.local>
2012-02-19 11:10:05 -0800

    Initial Commit

---
+++ GalleryOnATab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GalleryOnATab
+//
+//  Created by John Mattos on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GalleryOnATab/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GalleryOnATab
+//
+//  Created by John Mattos on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
